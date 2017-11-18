d9abaf9d470ec0d70bb16b8dcba9b7dd3f3a900f
Jachaplin <jakealanchaplin@yahoo.com>
2017-11-18 10:39:42 -0800

    resolved git issue

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burger_db;
+
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('bacon double with cheese', false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('mushroom and swiss', false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('bbq and onion rings', false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('turkey avocado', false, CURRENT_TIMESTAMP);
